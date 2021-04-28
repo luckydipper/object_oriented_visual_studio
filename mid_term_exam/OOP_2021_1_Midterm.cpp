4efbbffb5593ddf700d22cb2109e1b0672dbe371
Luckydipper <qhrqufdlek@naver.com>
2021-04-28 12:23:24 +0900

    [add] - midterm environment

---
+++ mid_term_exam/OOP_2021_1_Midterm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+void midterm_ex();
+
+#ifdef WIN32
+#ifdef _DEBUG
+#pragma comment(lib, "OOP_2021_1_Midterm_LibD.Lib")
+#else
+#pragma comment(lib, "OOP_2021_1_Midterm_Lib.Lib")
+#endif
+#else
+#ifdef _DEBUG
+#pragma comment(lib, "OOP_2021_1_Midterm_Lib64D.Lib")
+#else
+#pragma comment(lib, "OOP_2021_1_Midterm_Lib64.Lib")
+#endif
+#endif
+
+int main()
+{
+	midterm_ex();
+	std::cout << "It works!";
+}
