c248465ef0ffd6cad169a0a85efaa7247e256b2d
Luckydipper <qhrqufdlek@naver.com>
2021-06-07 20:55:09 +0900

    [add] - oop lec 12 B-3

--- object_oriented_programing/oop lec 12.cpp
+++ object_oriented_programing/oop lec 12.cpp
@@ -1,98 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <cassert>
-#include "oop lec 12.h"
-
-double Shape::PI = 3.141592;
-
-bool hasNum(const std::string a[], int size, std::string figure) {
-	for (int i = 0; i < size; i++) {
-		if (a[i] == figure) {
-			return true;
-		}
+class Base {
+	int x;
+protected:
+	std::vector<int> Data;
+public:
+	Base(int x) :x(x) {
+		Data = std::vector<int>(x);
+	};
+	void print() { std::cout << x << std::endl; }
+ 	void setN(int repeat, int offset) {
+		if (repeat < x) {Data[repeat] = offset;}
+	};
+	void printData() {
+		for (const int element : Data)
+			std::cout << element << " ";
+		std::cout << "\n Data print complete" << std::endl;
 	}
-	return false;
-}
-
-TwoDemension::TwoDemension(std::string status, double x, double y) : status(status), x(x), y(y){
-	const std::string* check_label = this->status_label;
-	int size = sizeof(this->status_label) / sizeof(std::string);
-	bool adequent_figure = hasNum(check_label, size, status);
-	assert(adequent_figure && "status is not Triangle or Rectangule");
-	std::cout << status << " object has been made" << std::endl;
 };
-TwoDemension::TwoDemension(std::string status, double r) : status(status), r(r) {
-	const std::string* check_label = this->status_label;
-	int size = sizeof(this->status_label) / sizeof(std::string);
-	bool adequent_figure = hasNum(check_label, size, status);
-	assert(adequent_figure && "status is not a Circle ");
-	std::cout << status << " object has been made" << std::endl;
+class Derived : public Base {
+public:
+	Derived(int x) : Base(x) {};	
+	void insert(int elem) {
+		Base::Data.push_back(elem);
+	}
 };
-
-
-void TwoDemension::area_calculation() {
-	if (this->status == std::string("Triangle")) {
-		area = this->x * this->y * 1 / 2;
-		std::cout << this -> status << " area calcuated." << std::endl;
-	}
-	else if (this->status == std::string("Rectangule")) {
-		area = this->x * this->y;
-		std::cout << this->status << " area calcuated." << std::endl;
-	}
-	else if (this->status == std::string("Circle")) {
-		area = this->r * this->r * this->PI;
-		std::cout << this->status << " area calcuated." << std::endl;
-	}
-}
-
-void TwoDemension::print() {
-	std::cout << "Area of " << this->status << " is " << this->area << std::endl;
-}
-
-TreeDemension::TreeDemension(std::string status, double x, double y, double z) : status(status), x(x), y(y), z(z) {
-	const std::string* check_label = this->status_label_3D;
-	int size = sizeof(this->status_label_3D) / sizeof(std::string);
-	bool adequent_figure = hasNum(check_label, size, status);
-	assert(adequent_figure && "status is not Triangle or Rectangule");
-	std::cout << status << " object has been made" << std::endl;
-}
-
-TreeDemension::TreeDemension(std::string status, double r) : status(status), r(r) {
-	const std::string* check_label = this->status_label_3D;
-	int size = sizeof(this->status_label_3D) / sizeof(std::string);
-	bool adequent_figure = hasNum(check_label, size, status);
-	assert(adequent_figure && "status is not a Circle ");
-	std::cout << status << " object has been made" << std::endl;
-}
-
-void TreeDemension::area_calculation() {
-	if (this->status == std::string("Cube")) {
-		area = this->x * this->y * this->z;
-		std::cout << this->status << " area calcuated." << std::endl;
-	}
-
-	else if (this->status == std::string("Sphere")) {
-		area = 4. / 3. *this->r * this->r * this->r* this->PI;
-		std::cout << this->status << " area calcuated." << std::endl;
-	}
-}
-
-void TreeDemension::print() {
-	std::cout << "Area of " << this->status << " is " << this->area << std::endl;
-}
-
 
 int main() {
-	TwoDemension triange(std::string("Triangle"), 8., 4.);
-	triange.area_calculation();
-	triange.print();
-	TwoDemension circle(std::string("Circle"), 3.);
-	circle.area_calculation();
-	circle.print();
-	TreeDemension sphere(std::string("Sphere"), 3.);
-	sphere.area_calculation();
-	sphere.print();
-	return 0;
+	Base b1(2), b2(10);
+	b1.print();		// 2
+	b2.print();		// 10
+	for (int i = 0; i < 5; i++) {
+		b1.setN(i, (i + 1) * 20);//0,20//1,40//2,60
+		b2.setN(i, (i + 1) * 10);//0,10//1,20//2,30//3,40//4,50
+	}
+	b1.printData();  // 20 40
+	b2.printData();  // 10 20 30 40 50 0 0 0 0 0
+
+	Derived d(5);
+	d.print();		// 5
+	d.printData();		// 0 0 0 0 0
+	for (int i = 0; i < 10; i++) {
+		d.setN(i, (i + 1) * 3);
+	}
+	d.printData();	// 3 6 9 12 15
+	d.insert(99);	// "Base" class does not have "insert" method.
+	d.printData();	// 3 6 9 12 15 99
 
 }
