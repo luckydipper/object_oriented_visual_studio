959d114278bb8b6e06085dc1b257b71f4a5bb9e2
Luckydipper <qhrqufdlek@naver.com>
2021-07-05 22:13:04 +0900

    [end] - 독하게 시작하는 C언어

--- c_study/string_processing.c
+++ c_study/string_processing.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int print_foo(char name[]) {
+	printf("foo %s", name);
+	return 3;
+}
+
+// char *names = "HeeSung Kim";
+// int (*ptr_print_foo)(char *name) = print_foo;
+// ptr_print_foo(names);
+// errno_t err;
+// FILE *pt_file = NULL;
+// err = fopen_s(&pt_file, "CON", "w");
+// fprintf(pt_file, "hello world");
+// fclose(pt_file);
+
+void getName(char const name[], int n_size) {
+	printf("Input your name\n");
+	char szBuffer[32] = { 0 };
+	gets_s(szBuffer, sizeof(szBuffer));
+	strcpy_s(name, n_size, szBuffer);
+};
 
 int main() {
-	char* strs;
-	strs = "AAABBC";
-	printf("%str", strs);
+	//obtimize example
+	char name[32] = { 0 };
+	FILE* ptr_input_buff = stdin;
+	scanf_s("%s", name, sizeof(name));
 
-	int scores[10];
-	for (int index = 0; index < 10; index++){
-		scores[index] = index;
-	};
+	printf("%s", name);
+	getchar();
+	gets_s(name, sizeof(name));
+	getName(name, sizeof(name));
+	puts(name);
 	return 0;
 }
